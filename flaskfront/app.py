8b0cabcfe71e15311a79d3e2395e3c154c914024
riyli <richard@riy.li>
2021-02-21 08:18:46 -0800

    finalized build, working pdf generation and flask frontend/backend linking

--- flaskfront/app.py
+++ flaskfront/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify, request
 import json
 import requests
 import Scnr
+import os
 
 studyguide = Scnr.Scnr()
 
@@ -21,18 +22,18 @@ def test():
 @app.route('/add', methods = ['POST'])
 def addURL():
 
-    # data = json.loads(request.data)
-    # url_to_add = data['url']
-    # studyguide.add_url(url_to_add)
+    data = json.loads(request.data)
+    url_to_add = data['url']
+    studyguide.add_url(url_to_add)
     # studyguide.to_pdf()
 
     # result = request.form;
-    msg = {'greeting':"test"}
+    # msg = {'greeting':"test"}
     # print(request.form["url"])
     # studyguide.add_url(url)
-    # msg = {'greeting':studyguide.count}
+    msg = {'greeting':studyguide.count}
     return jsonify(msg)
 
-@app.route('/add', methods = ['GET'])
+@app.route('/gen', methods = ['GET'])
 def generatePDF():
     studyguide.to_pdf()
